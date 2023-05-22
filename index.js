dfa682f6d010776d1f0433158b349108c686ab99
willkelly90 <willkelly283@gmail.com>
2023-05-21 22:32:22 -0400

    Update index.js

--- index.js
+++ index.js
@@ -13,6 +13,31 @@ class Timer{
 	}
 }
 
+function notify(message) {
+  if (!("Notification" in window)) {
+    // Check if the browser supports notifications
+    alert("This browser does not support desktop notification");
+  } else if (Notification.permission === "granted") {
+    // Check whether notification permissions have already been granted;
+    // if so, create a notification
+    const notification = new Notification(message);
+    // …
+  } else if (Notification.permission !== "denied") {
+    // We need to ask the user for permission
+    Notification.requestPermission().then((permission) => {
+      // If the user accepts, let's create a notification
+      if (permission === "granted") {
+        const notification = new Notification(message);
+        // …
+      }
+    });
+  }
+
+  // At last, if the user has denied notifications, and you
+  // want to be respectful there is no need to bother them anymore.
+}
+
+
 function EmptyTimerOptions(){
 	document.querySelector("#timers").innerHTML = '';
 }
@@ -87,11 +112,10 @@ function PrintTimers(){
 			while(true){
 				
 				var timeLapse = (new Date() - self.runningStart)/1000;
-				// console.log(timeLapse);
-				
+								
 				if(timeLapse>=segundos){
 					document.querySelector(`#runningTimers #${self.timerID}`).dataset.finished = true;
-					console.log("Terminó");
+					notify(`El timer de ${self.name} finalizó.`);
 					self.running = false;
 					break;
 				}
